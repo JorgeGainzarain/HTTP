22411c200b58e22f203a7e882dc56d9467d7e579
jorge <alu.136058@usj.es>
2024-04-17 17:03:23 +0200

    AA

--- client.js
+++ client.js
@@ -11,17 +11,24 @@ selectHost()
   let host = arr[1];
   client.connect(port, host, () => {
     console.log("Connected to server ", host , ":", port);
-    menuGestion(true);
   });
 })
 
 // requests process
+
+client.on("connect", () => {
+  menuGestion(true);
+})
 
 client.on("data", (data) => {
   console.log("\n\nReceived:\n " + data.toString());
 
   menuGestion();
 });
+
+client.on("error", (err) => {
+  console.log(err.name);
+})
 
 // Handle the close event
 client.on("close", () => {
